2d6bced76e60c0026cda7f0c535e232f16284685
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-09-11 03:02:21 +0300

    add file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const input = document.querySelector('input#searchById');
+
+        fetch(`http://localhost:3000/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('sectin#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+    })
   
 }
 
